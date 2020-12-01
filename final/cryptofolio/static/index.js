6948cd73d7cffee7e656758571164a6c11772a5b
Martijncvv <martijncvv@gmail.com>
2020-12-01 22:14:38 +0100

    Added log-in pop-up screen; still in progress

--- final/cryptofolio/static/index.js
+++ final/cryptofolio/static/index.js
@@ -1,30 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // search function
+    document.querySelector('#submit_search').addEventListener('click', general_info);
+    // login button
+    document.querySelector('#login').addEventListener('click', login);
+    // close field button
+    document.querySelector('#close_field').addEventListener('click', close_field);
+        
 
-
-// Select the submit button and input to be used later
-const submit = document.querySelector('#submit');
-const coin_name = document.querySelector('#search_value');
-
-// Disable submit button by default:
-submit.disabled = true;
-
-// Listen for input to be typed into the input field
-coin_name.onkeyup = () => {
-    if (coin_name.value.length > 0) {
-        submit.disabled = false;
+    // Select the submit button and input to be used later
+    const submit_search = document.querySelector('#submit_search');
+    const coin_name = document.querySelector('#search_value');
+    // Disable submit button by default:
+    submit_search.disabled = true;
+    // Listen for input to be typed into the input field
+    coin_name.onkeyup = () => {
+        if (coin_name.value.length > 0) {
+            submit_search.disabled = false;
+        }
+        else {
+            submit_search.disabled = true;
+        }
     }
-    else {
-        submit.disabled = true;
+
+    general_info("bitcoin");
+
+});
+
+
+function general_info(coin) {
+    document.querySelector("#loginfield").style.display = 'none';
+
+    // get value from form
+    let coin_name = document.querySelector('#search_value');
+    if (coin_name.value !== ""){
+        coin = coin_name.value;
     }
-}
-
-// Listen for submission of form
-document.querySelector('form').onsubmit = () => {
-
-    // Find the task the user just submitted
-    const coin = coin_name.value;
-
-    // Create a list item for the new task and add the task to it
+    
+    // Create a list item
     let li = document.createElement('li');
     li.innerHTML = coin;
 
@@ -35,38 +47,30 @@ document.querySelector('form').onsubmit = () => {
     coin_name.value = '';
 
     // Disable the submit button again:
-    submit.disabled = true;
+    submit_search.disabled = true;
 
-    // Stop form from submitting
    
-
     fetch("https://api.coingecko.com/api/v3/coins/" + coin + "?localization=false&tickers=true&market_data=true&community_data=false&developer_data=false&sparkline=false")
     .then(response => response.json())
     .then(data => {
         
-        console.log(data)
-      
+        console.log(data)  // KANN WEG
+        // Add data to elements
+        
         document.getElementById("coin_info_name").innerHTML = "Name: " + data.name;
-        document.getElementById("coin_info_ticker").innerHTML = "Ticker: " + data.symbol;
-        document.getElementById("coin_info_price").innerHTML = "Price: " + data.market_data.current_price.usd;
-        document.getElementById("coin_info_marketcap").innerHTML = "Market cap: " + data.market_data.market_cap.usd;
+        document.getElementById("coin_info_ticker").innerHTML = "Ticker: $" + data.symbol;
+        document.getElementById("coin_info_price").innerHTML = "Price: $" + data.market_data.current_price.usd;
+        document.getElementById("coin_info_marketcap").innerHTML = "Market cap: $" + data.market_data.market_cap.usd;
         document.getElementById("coin_info_atl").innerHTML = "ATL: " + data.market_data.atl.usd + "   Date: " + data.market_data.atl_date.usd;
         document.getElementById("coin_info_ath").innerHTML = "ATH: " + data.market_data.ath.usd + "   Date: " + data.market_data.ath_date.usd;
 
-        // ath_date
     });
-    return false;
+}
 
-} });
+function login() {
+    document.querySelector("#loginfield").style.display = "block";
+}
 
-
-
-// function search_coin() {
-//     // get searchfield input
-//     var coin_name = document.getElementById("coin-search-form");
-
-//     // add new item in list
-//     const li = document.createElement('li');
-//     li.innerHTML = coin_name.elements[0].value;
-//     document.querySelector('#coin-list').append(li);
-// }
+function close_field() {
+    document.querySelector("#loginfield").style.display = "none";
+}
--- final/cryptofolio/views.py
+++ final/cryptofolio/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# login 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from .models import User, Portfolio, Coinholding, Settings
 
 def index(request):
     return render(request, "cryptofolio/index.html")
-    
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=email, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "cryptofolio/index.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "cryptofolio/index.html")
